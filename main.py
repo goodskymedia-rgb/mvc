68e135d5d6e8fb024d1470a0f756aec6067c15d7
Taras Gudzovskyi <goodskymedia@gmail.com>
2025-11-26 22:18:58 +0100

    updated main, added fastApi, added methods

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Start")
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {}
+
+@app.put("/")
+def root():
+    return {}
+
+@app.delete("/")
+def root():
+    return {}
+
+@app.post("/")
+def root():
+    return {}
